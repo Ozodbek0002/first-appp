660b80184d014cdbd71e2d72340f6de22201d435
Ozodbek <ozodbekozodov0002@gmail.com>
2025-04-24 16:39:06 +0500

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { log } from 'console';
+
+
+
+@Component({
+  selector: 'app-root',
+  imports: [ CommonModule,HttpClientModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent implements OnInit  {
+
+  title = 'Salom bu  Asosiy Componenta';
+  Items:any
+
+  constructor( private http: HttpClient) { }
+
+  getPosts() {
+    return    this.http.get('https://fakestoreapi.com/products');
+  }
+
+
+  ngOnInit(): void {
+  
+    this.getPosts().subscribe((data) => {
+
+     console.log(data);
+
+     this.Items = data
+     
+    });
+
+  }
+
+  
+
+
+
+}
