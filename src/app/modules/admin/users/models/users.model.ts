818708d1f674ccf402e971a26879f13b39b9859b
Ozodbek Ozodov <o.ozodov@uzinfocom.uz>
2025-06-04 11:33:25 +0500

    users model

---
+++ src/app/modules/admin/users/models/users.model.ts
@@ -0,0 +1,18 @@
+export interface Person {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
+
+export interface Users {
+  id: string;
+  name: string;
+  descroption: string;
+  phone: string;
+  address: string;
+  createdAt: Date;
+  dateOfBith: Date;
+  dateOfRegister: Date;
+  specialization: string;
+}
--- src/app/modules/admin/users/users/users.component.ts
+++ src/app/modules/admin/users/users/users.component.ts
@@ -1,39 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { log } from 'console';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzTableModule } from 'ng-zorro-antd/table';
-
-interface Person {
-  key: string;
-  name: string;
-  age: number;
-  address: string;
-}
+import { Person, Users } from '../models/users.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-users',
-  imports: [NzDividerModule, NzTableModule],
+  imports: [NzDividerModule, NzTableModule, NzButtonModule, NzIconModule, CommonModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css',
 })
 export class UsersComponent {
-  listOfData: Person[] = [
-    {
-      key: '1',
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-    },
-    {
-      key: '2',
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-    },
-    {
-      key: '3',
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sidney No. 1 Lake Park',
-    },
-  ];
+  listOfData: Users[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http
+      .get<Users[]>('https://683fe20d5b39a8039a55f6f2.mockapi.io/users/users')
+      .subscribe((data) => {
+        this.listOfData = data;
+      });
+  }
 }
